84c7399587dd8b5b3f9fb60c2d2e19849543efa3
GeorgeLinardis <glinardis@gmail.com>
2017-05-30 02:29:10 +0300

    solved title-case-sentence algorithm first time

---
+++ basic-algorithm-scripting/title-case-a-sentence.js
@@ -0,0 +1,31 @@
+/**
+ * FreeCodeCamp Challenge
+ * -------------------------
+ * Return the provided string with the first letter of each word capitalized.
+ * Make sure the rest of the word is in lower case.
+ * For the purpose of this exercise, you should also capitalize connecting
+ * words like "the" and "of".
+ *
+ * -------------------------
+ * @param  {string} str
+ * @return {string}
+ *        returns string with capitalized first letters
+ */
+"use strict";
+
+function titleCase(str) {
+
+    function capitalize(word) {
+        return word.charAt(0).toUpperCase()+word.slice(1,word.length);
+    }
+
+    let results=[];
+    str.toLowerCase().split(" ").forEach(function(element){
+        element = capitalize(element);
+        results.push(element);
+    });
+
+    return results.join(" ");
+}
+
+titleCase("sHoRt AnD sToUt");
