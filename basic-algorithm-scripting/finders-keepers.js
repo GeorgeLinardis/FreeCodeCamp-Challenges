d49390757e31ed84e6f979f44faccc3958e10b52
GeorgeLinardis <glinardis@gmail.com>
2017-05-30 00:17:48 +0300

    solved finders-keepers first time

---
+++ basic-algorithm-scripting/finders-keepers.js
@@ -0,0 +1,31 @@
+/**
+ * FreeCodeCamp Challenge
+ * -------------------------
+ * Create a function that looks through an array (first argument)
+ * and returns the first element in the array that passes a truth test
+ * (second argument). If no element passes the test, return undefined.
+ * -------------------------
+ * @param {Array} arr
+ *         contains number to be checked from func function
+ * @param {function} func
+ *         function checks if number provided is divided by 2
+ * @returns {number}
+ *         returns first number in the arr array which passes the test of func
+ *
+ */
+
+"use strict";
+
+function findElement(arr, func) {
+    let num = undefined;
+    for(let i=0;i<arr.length;i++){
+        num = func(arr[i])?arr[i]:undefined;
+        if (num){
+            return num;
+        }
+    };
+    return num;
+}
+
+findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; });
+
