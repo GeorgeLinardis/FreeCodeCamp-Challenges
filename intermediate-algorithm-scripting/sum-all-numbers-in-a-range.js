609fa7e538e20bcc8c7b3e1e5329239f0527112b
GeorgeLinardis <glinardis@gmail.com>
2017-05-30 18:19:42 +0300

    solved sum-all-numbers-in-range algorithm first time

---
+++ intermediate-algorithm-scripting/sum-all-numbers-in-a-range.js
@@ -0,0 +1,25 @@
+/**
+ * FreeCodeCamp Challenge
+ * -------------------------
+ * We'll pass you an array of two numbers.
+ * Return the sum of those two numbers and all numbers between them.
+ *
+ * -------------------------
+ * @param {Array} arr
+ *        includes min and max of the range of numbers added
+ * @return {number}
+ *        return sum of all numbers
+ */
+
+"use strict";
+
+function sumAll(arr) {
+    let min,max,result=0;
+    arr = arr[0]>arr[1]?[max=arr[0],min=arr[1]]:[max=arr[1],min=arr[0]];
+    for(let i=min;i<=max;i++){
+        result+=i;
+    }
+    return result;
+}
+
+console.log(sumAll([1, 4]));
