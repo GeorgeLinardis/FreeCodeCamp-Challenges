3299e4014c370a3eb52d86c8910b10362abd1ebc
GeorgeLinardis <glinardis@gmail.com>
2017-06-01 23:01:30 +0300

    almost seeing the end of js challenges

---
+++ Basic-javascript/return-a-value-from-a-function-with-return.js
@@ -0,0 +1,17 @@
+
+// Example
+function minusSeven(num) {
+    return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive(arg){
+    return 5*arg;
+}
+
+
+
+
+
+
---
+++ Basic-javascript/shopping-list.js
@@ -0,0 +1,16 @@
+
+var myList = [
+    ["apples",3],
+
+    ["lemons",2],
+
+    ["yoghurt",11],
+
+    ["chocolate",53],
+
+    ["bananas",22],
+
+    ["lollipop",1],
+];
+
+
