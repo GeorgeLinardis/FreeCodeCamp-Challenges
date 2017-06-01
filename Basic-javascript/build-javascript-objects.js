ba3159a40cb9aadcf1acb8f7aa5781b3122df739
GeorgeLinardis <glinardis@gmail.com>
2017-06-02 00:31:17 +0300

    finished js basic challenges

---
+++ Basic-javascript/build-javascript-objects.js
@@ -0,0 +1,18 @@
+
+// Example
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+    "name": "myDog",
+    "legs": 4,
+    "tails": 2, //alien dog
+    "friends": ["Bob", "Dob"]
+
+};
---
+++ Basic-javascript/manipulating-complex-objects.js
@@ -0,0 +1,26 @@
+
+var myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CS",
+            "8T",
+            "LP" ],
+        "gold": true
+    },
+    // Add record here
+    {
+        "artist": "Michael Jackson",
+        "title": "Bad",
+        "release_year": 1980,
+        "formats": [
+            "CS",
+            "8T",
+            "LP" ],
+        "gold": true
+    }
+
+];
+
