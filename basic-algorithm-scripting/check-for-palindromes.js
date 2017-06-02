196d7b38ce7b8d9d88b2f46bd66da8855f4f484c
GeorgeLinardis <glinardis@gmail.com>
2017-06-02 13:53:54 +0300

    solved check for palindrome algorithm first time

---
+++ basic-algorithm-scripting/check-for-palindromes.js
@@ -0,0 +1,51 @@
+/**
+ * FreeCodeCamp Challenge
+ * -------------------------
+ * Return true if the given string is a palindrome. Otherwise, return false.
+ *
+ * A palindrome is a word or sentence that's spelled the same way both forward
+ * and backward, ignoring punctuation, case, and spacing.
+ *
+ * Note
+ * You'll need to remove all non-alphanumeric characters (punctuation,
+ * spaces and symbols) and turn everything lower case in order to check for
+ * palindromes.
+ *
+ * We'll pass strings with varying formats, such as "racecar", "RaceCar",
+ * and "race CAR" among others.
+ *
+ * We'll also pass strings with special symbols, such as "2A3*3a2",
+ * "2A3 3a2", and "2_A3*3#A2".
+ *
+ * -------------------------
+ * @param {string} str
+ *
+ * @return {boolean}
+ *
+ */
+"use strict";
+
+function palindrome(str) {
+    // Good luck
+
+    let phrase = str.toLowerCase().replace(/[\W_]/g,"");
+    // \W in all non word characters
+    // _ adds underscore to the characters searched for
+    //g is the modifier that tells it not to stop at the first match
+
+    for (let i=0;i<phrase.length;i++){
+        if(phrase[i] !== phrase[phrase.length-1-i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+
+
+
+
+
+
