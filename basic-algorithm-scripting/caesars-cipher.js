eba789d89f59705d73d13649a039d4787ba75cbc
GeorgeLinardis <glinardis@gmail.com>
2017-06-02 14:55:09 +0300

    solved seek-and-destroy algorithm first time

---
+++ basic-algorithm-scripting/caesars-cipher.js
@@ -0,0 +1,3 @@
+/**
+ * Created by george on 2/6/2017.
+ */
---
+++ basic-algorithm-scripting/seek-and-destroy.js
@@ -0,0 +1,27 @@
+/**
+ * FreeCodeCamp Challenge
+ * -------------------------
+ * You will be provided with an initial array (the first argument
+ * in the destroyer function), followed by one or more arguments.
+ * Remove all elements from the initial array that are of the same
+ * value as these arguments.
+ *
+ * -------------------------
+ * @param  {Array} arr
+ * @return {Array}
+ *
+ */
+
+"use strict";
+
+function destroyer(arr) {
+    // Remove all the values
+    let removeValues = Array.from(arguments).slice(1);
+
+    return arguments[0].filter(function(element){
+        return removeValues.indexOf(element)===-1;
+    });
+}
+
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
