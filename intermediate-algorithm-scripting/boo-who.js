53d6fe65440c586d63a8243ac1178934839c1fd5
GeorgeLinardis <glinardis@gmail.com>
2017-06-02 15:07:39 +0300

    solved boo-whoo algorithm first time

---
+++ intermediate-algorithm-scripting/boo-who.js
@@ -0,0 +1,20 @@
+/**
+ * FreeCodeCamp Challenge
+ * -------------------------
+ * Check if a value is classified as a boolean primitive. Return true or
+ * false.
+ *
+ * Boolean primitives are true and false.
+ *
+ * -------------------------
+ * @param {Boolean} bool
+ * @return {Boolean}
+ *        returns if bool is classified as boolean
+ */
+
+function booWho(bool) {
+    // What is the new fad diet for ghost developers? The Boolean.
+    return typeof(bool)==="boolean";
+}
+
+booWho(null);
