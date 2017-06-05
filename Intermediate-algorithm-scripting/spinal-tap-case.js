bb1641b61ff6c41c3e369037a0cbeb3448127bad
GeorgeLinardis <glinardis@gmail.com>
2017-06-05 21:09:38 +0300

    solved spinal-tap-case algorithm first time

---
+++ Intermediate-algorithm-scripting/spinal-tap-case.js
@@ -0,0 +1,25 @@
+/**
+ * FreeCodeCamp Challenge
+ * -------------------------
+ * Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+ *
+ * -------------------------
+ * @param {string} str
+ * @return {string}
+ *
+ */
+"use strict";
+
+function spinalCase(str) {
+
+
+
+    return str.replace(/[ _]/g,'-'). //replaces spaces and underscore with -
+    replace(/([a-z])([A-Z])/g,"$1-$2" ).
+    //replaces when finds lowerCase letter and next to it uppercase with a - between them
+
+    toLowerCase();
+}
+
+spinalCase('thisIsSpinalTap');
+
