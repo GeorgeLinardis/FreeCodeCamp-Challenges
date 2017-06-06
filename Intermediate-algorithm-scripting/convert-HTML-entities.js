3ec687354805570dba8f19a5e22a67e42d5259fc
GeorgeLinardis <glinardis@gmail.com>
2017-06-07 01:42:42 +0300

    solved convert-HTML-entities algorithm first time

---
+++ Intermediate-algorithm-scripting/convert-HTML-entities.js
@@ -0,0 +1,38 @@
+/**
+ * FreeCodeCamp Challenge
+ * -------------------------
+ * Convert the characters &, <, >, " (double quote), and ' (apostrophe),
+ * in a string to their corresponding HTML entities.
+ *
+ * Remember to use Read-Search-Ask if you get stuck. Try to pair program.
+ * Write your own code.
+ *
+ * -------------------------
+ * @param {string} str
+ * @return {string}
+ *
+ */
+"use strict";
+
+function convertHTML(str) {
+    // &colon;&rpar;
+
+    function replacer(char,pos,phras,el4,el5){
+        // el1 character el2 position
+        switch (char) {
+            case "&":
+                return "&amp;";
+            case "<":
+                return "&lt;";
+            case ">":
+                return "&gt;";
+            case "\"":
+                return "&quot;";
+            case "'":
+                return "&apos;";
+        }
+    }
+    return str.replace(/[&<>"']/g,replacer);
+}
+
+console.log(convertHTML('Stuff in "quotation marks"'));
