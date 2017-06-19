2c48ee746cb0cf21bae0402216edf966ca017a6f
GeorgeLinardis <glinardis@gmail.com>
2017-06-19 05:22:51 +0300

    finished wiki search challenge

---
+++ Intermediate-Front-End-Development-Projects/wikipedia-viewer/README.md
@@ -0,0 +1,22 @@
+
+freeCodeCamp Wiki search
+-------------------------
+
+Through this challenge I created a search page for wiki articles.
+
+You can either ask for certain articles based on your searching or ask for a random one.
+
+Here is a preview..
+
+![Wiki search](wiki-preview.PNG)
+
+Image is taken from [Pixabay.com](https://www.pixabay.com/).
+And of course API is through wikipedia .
+
+You can the wiki search live in [Codepen](https://codepen.io/GeorgeLin/pen/ZyWrOW).
+
+Keep coding,
+
+George
+
+
--- Intermediate-Front-End-Development-Projects/wikipedia-viewer/code.js
+++ Intermediate-Front-End-Development-Projects/wikipedia-viewer/code.js
@@ -3,30 +3,50 @@ $(document).ready(function(){
     console.log("lock and loaded")
     let $randomButton = $("#button-random");
     let $searchButton = $("#button-search");
+    let basicUrl = "https://en.wikipedia.org/w/api.php?";
+    let $queryResults = $("#query-results");
+    let $queryList = $("#query-list");
+    let queryDesc = {};
 
 
+    function setData(element,title,description,link){
+        $("<a href="+"'"+link+"'"+";><li>"+title+"</li></a>").appendTo(element).addClass("query-list-item");
+        queryDesc[title]=description;
+
+    }
+
+    function getData(query){
+        basicUrl+="action=opensearch";
+        basicUrl+="&search="+query;
+        basicUrl+="&format=json";
+
+        $.ajax({
+            url:basicUrl,
+            dataType:"jsonp",
+            type:"GET",
+            success: function(data){
+                $('.query-list-item').remove();
+                data[1].forEach(function(element,iter){
+                    setData($queryList,element,data[2][iter],data[3][iter]);
+                });
+            }
+        }).done(function(){
+            $(".query-list-item").hover(function(event){
+                $("#result-description").text(queryDesc[event.target.text]);
+            });
+        })
+    }
 
 
-    $randomButton.click(function(){
-        console.log("random button clicked");
-        window.open('http://en.wikipedia.org/wiki/Special:Random',"_blank");
+    $randomButton.click(function(){        window.open('http://en.wikipedia.org/wiki/Special:Random',"_blank");
     });
 
     $searchButton.click(function(){
-        console.log("search button clicked");
-          $(".container").addClass("container-top",2000)
-    })
-
-
-
-
-
-
-
-
-
-
-
+        $("#main-container").addClass("main-container-top");
+        $("#query-results").addClass("query-expanded");
+        let query = $("#text-input").val();
+        getData(query);
+    });
 
 
 });
