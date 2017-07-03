21e1671d4e2fdfaa48d1588f5d6a3b9b125a8949
GeorgeLinardis <glinardis@gmail.com>
2017-07-03 22:30:26 +0300

    added basic game functions

--- Advanced-Front-End-Development-Projects/tic-tac-toe/js/basic.js
+++ Advanced-Front-End-Development-Projects/tic-tac-toe/js/basic.js
@@ -1,9 +1,54 @@
 $('document').ready(function(){
     "use strict";
+    let playerSymbol="X",computerSymbol="O",
+        playerScore=0,computerScore = 0,turn="player";
 
-    console.log('all ok')
+   class Game {
+
+       constructor(playerSymbol,computerSymbol){
+           this.tileSymbols = $(".symbol");
+           this.playerScore = $("#player-score-number")[0];
+           this.computerScore = $("#computer-score-number")[0];
+       }
+
+       initializeGame(){
+           console.log("initialize game!");
+           //set all symbols to ""
+           for(let i=0;i<this.tileSymbols.length;i++){
+               this.tileSymbols[i].innerText="";
+           }
+       }
+
+       playerChooseSymbol(){
+           $("#messages").css("display","flex");
+           $("#present-choices").css("display","flex");
+       }
+
+       showGameResult(result){
+           $("#messages").css("display","flex");
+           $("#present-game-result").css("display","flex");
+           $("#game-result")[0].innerText = result;
+       }
+
+       messageDissapear(){
+           $("#messages").css("display","none");
+       }
+
+       resetGame (){
+           console.log("reset game!");
+           this.playerScore.innerText=0;
+           this.computerScore.innerText=0;
+           this.playerChooseSymbol();
+           this.initializeGame();
+
+       }
+
+   }
+
+  let myApp = new Game;
 
 
+   myApp.resetGame();
 
 
-})
+});
